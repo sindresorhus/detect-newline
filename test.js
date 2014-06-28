495478b77dc43b07db0a096d6ac186c832c7ccc9
Sindre Sorhus <sindresorhus@gmail.com>
2014-06-28 16:32:48 +0200

    init

---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+var assert = require('assert');
+var detectNewline = require('./');
+
+it('should return the used newline character', function () {
+	assert.equal(detectNewline('foo\r\nbar\r\nbaz\n'), '\r\n');
+	assert.equal(detectNewline('foo\nbar\nbaz\r\n'), '\n');
+	assert.equal(detectNewline('foo\nbar\r\n'), '\n');
+});
