fe837f3a3e4a9082e49cbb90afeeef886511f954
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2015-01-24 19:54:15 +0800

    Close #1 PR: Count newlines separately even if they are side by side.

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 var detectNewline = require('./');
 
 it('should return the used newline character', function () {
+	assert.equal(detectNewline('foo\r\nbar\r\nbaz\n\n\n'), '\n');
 	assert.equal(detectNewline('foo\r\nbar\r\nbaz\n'), '\r\n');
 	assert.equal(detectNewline('foo\nbar\nbaz\r\n'), '\n');
 	assert.equal(detectNewline('foo\nbar\r\n'), '\n');
